1e1ace11d372d17213f54bab23a0277e342101ee
César <cesarlpb@gmail.com>
2024-05-08 18:19:45 +0200

    Ejercicio 02

--- js/cliente/ejercicios/02.variables/02.js
+++ js/cliente/ejercicios/02.variables/02.js
@@ -13,3 +13,12 @@ P = 40
 A = 100
 
 */
+
+let lado = 25;
+// perímetro = 4 * lado
+let P = 4 * lado;
+// área = lado * lado
+let A = lado * lado;
+
+document.write("<p>El perímetro es: ", P, "</p>");
+document.write("<p>El área es: ", A, "</p>");
