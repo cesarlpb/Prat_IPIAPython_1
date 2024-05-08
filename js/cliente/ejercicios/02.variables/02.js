80bba475c3e1e4ae417e87f28f7be9f08fb45287
César <cesarlpb@gmail.com>
2024-05-08 18:13:30 +0200

    Ejercicio 02
    código inicial

---
+++ js/cliente/ejercicios/02.variables/02.js
@@ -0,0 +1,15 @@
+/*
+En una variable tienes el lado de un cuadrado, debes escribir un programa que te calcule el área y el perímetro del cuadrado.
+
+El area la calculas como lado multiplicado por lado. El perímetro es la suma de los cuatro lados.
+
+input: lado
+outputs: perímetro, área
+
+Ejemplo:
+
+lado = 10
+P = 40
+A = 100
+
+*/
