c07bc217113f5accc371370050da4eea1bb83a09
César <cesarlpb@gmail.com>
2024-05-08 18:09:15 +0200

    Ejercicio 01

--- js/cliente/ejercicios/02.variables/01.js
+++ js/cliente/ejercicios/02.variables/01.js
@@ -17,3 +17,20 @@ output: 242
 
 */
 
+// Caso 1:
+let precio = 100; // €
+let iva = 21; // porcentaje
+let impuesto = ( iva / 100 ) * precio; // 21 € 
+let total = precio + impuesto;
+console.log(precio, iva, "total:", total)
+
+// Finalmente, escribimos el resultado en el id: ej-1-1
+document.getElementById("ej-1-1").innerText = total;
+
+// Caso 2:
+precio = 200;
+impuesto = ( iva / 100 ) * precio; // 42 €
+total = precio + impuesto;
+console.log(precio, iva, "total:", total)
+// Finalmente, escribimos el resultado en el id: ej-1-2
+document.getElementById("ej-1-2").innerText = total;
