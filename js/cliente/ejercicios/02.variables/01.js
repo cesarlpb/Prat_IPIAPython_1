c1de6614b42884ce7693d02cb5d0525a5a030e8e
César <cesarlpb@gmail.com>
2024-05-08 17:57:21 +0200

    Ejercicios de variables

---
+++ js/cliente/ejercicios/02.variables/01.js
@@ -0,0 +1,19 @@
+/*
+
+Has hecho una compra y sabes el precio del producto y su iva. Haz un script que te calcule el precio total que vas a pagar por tu compra. 
+
+Te recuerdo que para calcular el total debes sumar al precio el resultado de multiplicasr precio por el iva y dividir por 100.
+
+input: precio, iva
+output: total
+
+Ejemplo:
+
+input: 100, 21
+output: 121
+
+input: 200, 21
+output: 242
+
+*/
+
