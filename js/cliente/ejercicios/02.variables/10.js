9780eb36027b24ed22b1c1cabeaa7f44749f1ab1
César <cesarlpb@gmail.com>
2024-05-09 19:35:42 +0200

    Ejercicio 10
    código inicial

---
+++ js/cliente/ejercicios/02.variables/10.js
@@ -0,0 +1,11 @@
+/*
+Pues seguimos con los números. 
+
+Escribe un script  que dado un número nos devuelva true si es un número par y false si es un número impar. 
+
+No usar condiconales.
+
+- Hay que ver como se hace una comparación
+- El tipo boolean => true o false
+
+*/
