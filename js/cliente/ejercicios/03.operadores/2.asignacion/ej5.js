7431a9bed1ab68cf85d662dac2ed9ea1d2737f33
César <cesarlpb@gmail.com>
2024-05-16 17:50:37 +0200

    Ejercicio 05

--- js/cliente/ejercicios/03.operadores/2.asignacion/ej5.js
+++ js/cliente/ejercicios/03.operadores/2.asignacion/ej5.js
@@ -1,18 +1,29 @@
 // Ejercicio 05
 
-// Operaciones sobre una variable
+console.groupCollapsed('Ejercicio 05');
 
+// Operaciones sobre una variable
+// Valor inicial de 100:
 let saldo = 100;
+
+// Quitar 10%:
 saldo *= 0.9; // 90% de 100 -> 90
 // saldo = saldo * 0.9;
 // etc.
 
+// Restar 15:
 saldo -= 15; // 90 - 15 -> 75
+
+// Añadir 12.5%:
 saldo *= (1 + 12.5 / 100); // 84...
 
+// Dividir por 2:
 saldo /= 2; // 42...
+
+// Añadir 21%:
 saldo *= (1 + 21 / 100); // 51...
 
 console.log(saldo); // 51.046875
+console.groupEnd();
 
 // Conclusión: hay que tener cuidado con las operaciones aritméticas + asignación como *= porque si ponemos otra operación adcicional puede hacer otro cálculo.
