30e0f082cb0014bfafed3ffec618cadc5292ba5e
César <cesarlpb@gmail.com>
2024-05-23 15:40:35 +0200

    Quito carpeta js para colocar learn-js

--- js/cliente/01.sintaxis.js
+++
@@ -1,6 +0,0 @@
-// Sintaxis
-
-console.log('Hola mundo desde archivo externo!')
-
-// Descomentar para ver alerta:
-// alert('Hola mundo desde final de archivo externo!');
--- js/cliente/02.variables.js
+++
@@ -1,7 +0,0 @@
-// Variables
-
-console.log('Variables:')
-
-
-console.log('Final de variables.')
-
